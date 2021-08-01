177f428267424905b6dfe8cb874a6ea24c0d2433
bshpa <dr.bodys2012@gmail.com>
2021-08-01 11:00:44 +0300

    Migrate to Kotlin Flow

--- app/src/main/java/com/bshpanchuk/testyalantis/common/di/NetworkModule.kt
+++ app/src/main/java/com/bshpanchuk/testyalantis/common/di/NetworkModule.kt
@@ -5,7 +5,6 @@ import okhttp3.OkHttpClient
 import okhttp3.logging.HttpLoggingInterceptor
 import org.koin.dsl.module
 import retrofit2.Retrofit
-import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory
 import retrofit2.converter.gson.GsonConverterFactory
 
 val networkModule = module {
@@ -21,7 +20,6 @@ private fun provideRetrofit(okHttpClient: OkHttpClient): Retrofit {
         .baseUrl(BASE_URL)
         .client(okHttpClient)
         .addConverterFactory(GsonConverterFactory.create())
-        .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
         .build()
 }
 
